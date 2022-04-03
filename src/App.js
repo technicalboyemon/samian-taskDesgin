2126f60ee89876df154f60e1e69326d5c3ba1fbc
Emon Ahmed <technicalboyemon@gmail.com>
2022-04-03 14:35:07 +0600

    Only Desktop Design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import logo from "./Image/logo.png";
+import banner from "./Image/banner.png";
+import fb from "./Image/facebook.png";
+import go from "./Image/google.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="task">
+      <div className="background">
+        <img src={banner} alt="" />
+      </div>
+      <div className="loginCenter">
+        <div className="Login">
+          <img src={logo} alt="Logo" />
+          <p className="logoText">Explore new courses... Hurry up!!</p>
+          <input className="input" type="email" placeholder="Email Address" />
+          <input className="input" type="password" placeholder="Password" />
+          <p className="forgetText">Forget password?</p>
+          <button>LOG IN</button>
+          <span className="lineBreak"> or </span>
+          <div className="social-icon">
+            <div><img src={fb} alt="FB" /></div>
+            <div><img src={go} alt="GO" /></div>
+          </div>
+          <p className="logoText">
+            Don't have an account? <span className="linkText">Sing Up</span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
